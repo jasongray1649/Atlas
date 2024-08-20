13cd6ef4027e37ab97c03805746a8617826e71bf
Jason <jasongray1649@gmail.com>
2024-08-20 18:38:05 -0500

    Removed superfluous assets.

--- constants/images.js
+++ constants/images.js
@@ -1,10 +1,7 @@
 import profile from "../assets/images/profile.png"
 import thumbnail from "../assets/images/thumbnail.png"
 import cards from "../assets/images/cards.png"
-import path from "../assets/images/path.png"
 import logo from "../assets/images/logo.png"
-import logoSmall from "../assets/images/logo-small.png"
-import empty from "../assets/images/empty.png"
 import icon from "../assets/images/icon.png"
 import logofull from "../assets/images/logo_full.png"
 import byzantine from "../assets/images/byzantine.png"
@@ -13,10 +10,7 @@ export default {
 	profile,
 	thumbnail,
 	cards,
-	path,
 	logo,
-	logoSmall,
-	empty,
 	icon,
 	logofull,
 	byzantine,
